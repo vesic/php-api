ebe690894853ab3f2456839b791741bc0f2e8810
vesic <dusan.vesic@gmail.com>
2017-03-30 14:10:16 +0000

    openshift db config

--- model/database.php
+++ model/database.php
@@ -1,8 +1,19 @@
 <?php
+
+$host = getenv('OPENSHIFT_MYSQL_DB_HOST') ? getenv('OPENSHIFT_MYSQL_DB_HOST') : 'localhost';
+$db = getenv('OPENSHIFT_GEAR_NAME') ? getenv('OPENSHIFT_GEAR_NAME') : 'db1';
+$port = getenv('OPENSHIFT_MYSQL_DB_PORT') ? getenv('OPENSHIFT_MYSQL_DB_PORT') : '3306';
+$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME') ? getenv('OPENSHIFT_MYSQL_DB_USERNAME') : 'root';
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD') ? getenv('OPENSHIFT_MYSQL_DB_PASSWORD') : '';
+
+define('DSN', sprintf("mysql:host=%s;port=%s;dbname=%s", $host, $port, $db));
+define('USERNAME', $username);
+define('PASSWORD', $password);
+
 class Database {
-    private static $dsn = 'mysql:host=localhost;dbname=db1';
-    private static $username = 'root';
-    private static $password = '';
+    private static $dsn = DSN;
+    private static $username = USERNAME;
+    private static $password = PASSWORD;
     private static $db;
 
     private function __construct() {}
